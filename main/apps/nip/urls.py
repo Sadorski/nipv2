f9c89321ef0a65104233ab6a71d2b9672ce7240c
Sultan Sultani <sadorski@Sultans-MacBook-Pro.local>
2018-03-29 20:19:12 -0700

    initial commit

---
+++ main/apps/nip/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from . import views           # This line is new!
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^new_user/?$', views.new_user),      
+    url(r'^login/?$', views.login),
+    url(r'^strengths/?$', views.strength),
+    url(r'^socials/?$', views.socials),
+    url(r'^socials_process/?$', views.socials_process), 
+    url(r'^process/?$', views.process), 
+    url(r'^home/?$', views.home), 
+    url(r'^help_search/?$', views.help_search),
+    url(r'^user_search/?$', views.user_search),
+    url(r'^searching_user/?$', views.searching_user), 
+    url(r'^searching_help/?$', views.searching_help) 
+    
+]
